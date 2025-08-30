d8efd9ae31c6db07d843dc32b25c1a970798ed5e
shashanksah <shashanksah143@gmail.com>
2025-08-30 18:16:11 +0530

    added bundle analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: process.env.GENERATE_SOURCEMAP !== "false",
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
